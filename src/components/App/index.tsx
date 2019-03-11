1d5865f7252951836a981b043d50cd70dc05f37a
SeongjooJin <wlswntjd61@naver.com>
2019-03-11 23:07:42 +0900

    초기 설정

---
+++ src/components/App/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App: React.FunctionComponent = () => (
+    <p>Hello</p>
+);
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
+import 'antd/dist/antd.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { LocaleProvider } from 'antd';
+import ko_KR from 'antd/lib/locale-provider/ko_KR';
+import App from './components/App';
+import './GlobalStyles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+      <LocaleProvider locale={ko_KR}>
+        <App />
+      </LocaleProvider>,
+    document.getElementById('root') as HTMLElement
+  );
